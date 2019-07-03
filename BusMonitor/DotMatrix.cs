0413cd6026ec3220a6ad975914651439e4c7168f
carlozzer@gmail.com <carlozzer@gmail.com>
2019-07-03 23:06:05 +0200

    Implementando DotMatrix

---
+++ BusMonitor/DotMatrix.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusMonitor
+{
+    public class DotMatrix
+    {
+        char pixel => '*';
+
+        ConsoleColor prev_back;
+        ConsoleColor prev_fore;
+
+        int[] buffer = new int[Console.WindowWidth*Console.WindowHeight];
+
+
+        public DotMatrix() {
+
+            prev_back = Console.BackgroundColor;
+            prev_fore = Console.ForegroundColor;
+
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        ~DotMatrix() {
+
+            Console.BackgroundColor = prev_back;
+            Console.ForegroundColor = prev_fore;
+
+        }
+
+        ConsoleColor ResolveColor(int col) {
+
+            ConsoleColor ret = ConsoleColor.DarkGray;
+
+            switch (col)
+            {
+                case 1:
+                    ret = ConsoleColor.Red;
+                    break;
+                case 2:
+                    ret = ConsoleColor.Yellow;
+                    break;
+                default:
+                    ret = ConsoleColor.DarkGray;
+                    break;
+            }
+
+            return ret;
+
+        }
+
+        void RefreshBuffer() {
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            
+            for ( int i = 0; i < buffer.Length; i++) {
+
+                Console.ForegroundColor = ResolveColor(buffer[i]);
+                Console.Write( pixel );
+
+            }
+        }
+
+        public void Clear() {
+
+            Array.Clear( buffer , 0 , buffer.Length );
+            RefreshBuffer();
+        }
+
+    }
+}
--- BusMonitor/Program.cs
+++ BusMonitor/Program.cs
@@ -7,21 +7,24 @@ namespace BusMonitor
 
         static void Main ( string[] args ) {
 
-            EMTClient cli = new EMTClient();
-            string token = cli.Login( "carlozzer@gmail.com" , "carlo33er@GMAIL.COM" );
+            //EMTClient cli = new EMTClient();
+            //string token = cli.Login( "carlozzer@gmail.com" , "carlo33er@GMAIL.COM" );
 
-            
-            do
-            {
-                while (!Console.KeyAvailable)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Press ESC to stop");
-                    int seconds = cli.TimeArrivalBus("2705", "C2", token);
-                    Console.WriteLine($"C2 {(seconds / 60).ToString("00")}:{(seconds%60).ToString("00")}");
-                    System.Threading.Thread.Sleep(10000);
-                }
-            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            DotMatrix display = new DotMatrix();
+            display.Clear();
+
+            Console.ReadKey();
+            //do
+            //{
+            //    while (!Console.KeyAvailable)
+            //    {
+            //        Console.Clear();
+            //        Console.WriteLine("Press ESC to stop");
+            //        int seconds = cli.TimeArrivalBus("2705", "C2", token);
+            //        Console.WriteLine($"C2 {(seconds / 60).ToString("00")}:{(seconds%60).ToString("00")}");
+            //        System.Threading.Thread.Sleep(10000);
+            //    }
+            //} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
             
         }
 
