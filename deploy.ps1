be44cd57699b2e2f014f6577d6e3ea7a4aaee329
carlozzer@gmail.com <carlozzer@gmail.com>
2019-08-14 00:03:06 +0200

    scp funciona mejor que ftp

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,9 @@
-dotnet publish --configuration Release
-ftp -s:ftp.dat
+dotnet publish BusMonitor.Web/BusMonitor.Web.csproj --configuration Release
+
+#ftp mput es arcaico y no permite copiar directorios recursivamente
+# probamos scp ( ssh copy )
+#ftp -s:ftp.dat
+scp -rp BusMonitor.Web\bin\Release\netcoreapp2.1\publish\* carlozzer@busmon.westeurope.cloudapp.azure.com:~/bus
 
 #hemos generado un key-ge para acceder con ssh sin password
 #desde powershell ( si no es admin se cuelga )
@@ -8,7 +12,7 @@ ftp -s:ftp.dat
 #copia la clave en servidor remoto
  #cat ~/.ssh/id_rsa.pub | ssh carlozzer@busmon.westeurope.cloudapp.azure.com "cat >> ~/.ssh/authorized_keys"
 
-ssh carlozzer@busmon.westeurope.cloudapp.azure.com "sudo cp bus/* /var/www/busmon/"
+ssh carlozzer@busmon.westeurope.cloudapp.azure.com "sudo rsync -r bus/ /var/www/busmon"
 ssh carlozzer@busmon.westeurope.cloudapp.azure.com "sudo service nginx restart"
 ssh carlozzer@busmon.westeurope.cloudapp.azure.com 
 
