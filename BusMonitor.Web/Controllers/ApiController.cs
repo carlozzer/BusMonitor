673700f8a71aad13fb5d19635902581f32fe7049
carlozzer@gmail.com <carlozzer@gmail.com>
2020-11-02 22:13:31 +0100

    indicador si está obteniendo datos o no

--- BusMonitor.Web/Controllers/ApiController.cs
+++ BusMonitor.Web/Controllers/ApiController.cs
@@ -18,25 +18,33 @@ namespace BusMonitor.Web.Controllers
         [Route("api/arrival/cat/{category}/token/{token}/{time}")]
         public TimeTable Arrival ( string category , string token , string time )
         {
-            Console.Out.WriteLine("=====================");
-            Console.Out.WriteLine( $"START TimeArrivalBus ( {category} , {token} , {time} )");
-            Console.Out.WriteLine("=====================");
+            //Console.Out.WriteLine("=====================");
+            //Console.Out.WriteLine( $"START TimeArrivalBus ( {category} , {token} , {time} )");
+            //Console.Out.WriteLine("=====================");
 
             string root_url = $"{this.HttpContext.Request.Scheme}://{this.HttpContext.Request.Host.Value}";
             TimeTable model = TablesBLL.ArrivalTimes( category , token , root_url );
 
-            Console.Out.WriteLine("=====================");
-            Console.Out.WriteLine("TimeArrivalBus");
-            Console.Out.WriteLine("=====================");
-            Console.Out.WriteLine( model.ToXmlString() );
-            Console.Out.WriteLine("=====================");
+            //Console.Out.WriteLine("=====================");
+            //Console.Out.WriteLine("TimeArrivalBus");
+            //Console.Out.WriteLine("=====================");
+            //Console.Out.WriteLine( model.ToXmlString() );
+            //Console.Out.WriteLine("=====================");
 
-            Console.Out.WriteLine("=====================");
-            Console.Out.WriteLine( $"END TimeArrivalBus ( {category} , {token} )");
-            Console.Out.WriteLine("=====================");
+            //Console.Out.WriteLine("=====================");
+            //Console.Out.WriteLine( $"END TimeArrivalBus ( {category} , {token} )");
+            //Console.Out.WriteLine("=====================");
 
             return model;
         }
+
+        //public void AvailableRAM()
+        //{
+        //    ComputerInfo CI = new ComputerInfo();
+        //    ulong mem = ulong.Parse(CI.TotalPhysicalMemory.ToString());
+        //    lbl_Avilable_Memory.Text = (mem / (1024 * 1024) + " MB").ToString();
+        //}
+
 
     }
 }
